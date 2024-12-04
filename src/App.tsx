55063819fbc0b653491435198751b962d01c04fd
Daniil <daniil.shishaev@gmail.com>
2024-12-04 22:10:27 +0300

    card foundation and buttons props typification with styled

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import styled from "styled-components";
+import logo from "./assets/mainImage.jpg";
+import  './App.css';
+
+function App() {
+    return (
+        <StyledDiv>
+            <StyledCard>
+                <ImgStyled src={logo}/>
+                <StyledCardItems>
+                    <h1>Headline</h1>
+                    <p>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in
+                        venen.</p>
+                    <ButtonContainer>
+                        <ButtonStyled isActive={false}>See more </ButtonStyled>
+                        <ButtonStyled isActive={true}>Save</ButtonStyled>
+                    </ButtonContainer>
+                </StyledCardItems>
+            </StyledCard>
+        </StyledDiv>
+
+    );
+}
+
+const StyledDiv = styled.div`
+    width: 300px;
+    height: 350px;
+    padding: 50px;
+`
+const StyledCard = styled.div`
+    border: 0 solid #ccc;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+
+`
+const StyledCardItems = styled.div`
+    padding: 0 20px;
+
+    h1 {
+        margin: 20px 0;
+        font-size: 16px;
+    }
+
+    p {
+        width: 260px;
+        height: 40px;
+        margin: 8px 0;
+        color: #666;
+        font-size: 12px;
+    }
+`
+
+const ButtonContainer = styled.div`
+
+    margin: 19px 0 22px 0;
+    width: 300px
+`
+type propsType = {
+    isActive: boolean,
+}
+const ButtonStyled = styled.div<propsType>`
+            background-color: ${(props) => (props.isActive ? "white" : "#4E71FE")};
+            color: ${(props) => (props.isActive ? "#007bff" : "#fff")};
+            border-color: ${(props) => (props.isActive ? "##007bff" : "#4E71FE")};
+
+//             // background-color: #4E71FE;
+//             // color: #fff;
+//             // border-color: #4E71FE;
+//
+//     // &:hover {
+//     //     background-color: white;
+//     //     color: #007bff;
+//     //     border-color: #007bff;
+//     // };
+// }
+
+     border: solid 1px;
+     border-radius: 5px;
+     //cursor: pointer;
+     font-size: 10px;
+     width: 86px;
+     height: 30px;
+     //white-space: normal;
+     margin-right: 12px;
+    //
+    // &:hover {
+    //     background-color: white;
+    //     color: #007bff;
+    //     border-color: #007bff;
+    // }
+
+`
+const ImgStyled = styled.img`
+    border-radius: 10px;
+    width: 280px;
+    height: 170px;
+    margin: 10px 0 0 10px;
+    object-fit: cover;
+`
+
+export default App;
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+export const baseTheme = {
+    colors: {
+        primary: '#7986cb',
+        secondary: '#2b2b2b',
+        success: '#4caf50',
+        danger: '#f44336 ',
+
+        bg: '#E5E4E8',
+        font: '#19191B',
+    }
+}
